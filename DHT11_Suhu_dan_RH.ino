b765d68b09f96c0cf5b3d6ca972b15f65a071cb9
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-05 10:49:35 +0700

    Update and rename DHT_Suhu_dan_RH.ino to DHT11_Suhu_dan_RH.ino

---
+++ DHT11_Suhu_dan_RH.ino
@@ -0,0 +1,21 @@
+#include <dht.h>
+#define sensor A0
+dht DHT;
+
+void setup() {
+  Serial.begin(9600);
+  delay(500);
+  Serial.println("Sensor Suhu dan Kelembaban Udara dengan DHT11\n\n");
+  delay(1000);
+}
+
+void loop() {
+  DHT.read11(sensor);
+  Serial.print("Kelembaban Udara = ");
+  Serial.print(DHT.humidity);
+  Serial.print("% ");
+  Serial.print("Suhu = ");
+  Serial.print(DHT.temperature);
+  Serial.println(" C ");
+  delay(1000);
+}
