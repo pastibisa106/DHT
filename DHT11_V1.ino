ef0500bb4b93213d1b8dd989690aa692370065f9
pastibisa106 <35872007+pastibisa106@users.noreply.github.com>
2025-05-05 10:47:49 +0700

    Add files via upload

---
+++ DHT11_V1.ino
@@ -0,0 +1,19 @@
+#include "DHTesp.h"
+DHTesp dht;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("RH (%)\t\tSuhu (C)");
+    dht.setup(14, DHTesp::DHT11); // GPIO14 (D5)
+}
+void loop() {
+    //delay(dht.getMinimumSamplingPeriod() + 100);
+    float rh = dht.getHumidity();
+    float suhu = dht.getTemperature();
+    Serial.print("RH (%): ");
+    Serial.print(rh, 1);
+    Serial.print("\t");
+    Serial.print("Suhu (C): ");
+    Serial.println(suhu, 1);
+    delay(2000);
+}
